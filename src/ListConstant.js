7e4b285596dcfda6a7318118c8d6429c40a6ac09
vasanth <vasanthsbtect@gmail.com>
2024-12-14 11:09:08 +0530

    aboutus

---
+++ src/ListConstant.js
@@ -0,0 +1,32 @@
+export const SERVICES = [
+    {
+        image: './AboutUs/hearing.jpg',
+        title: 'Hearing Aid Trial',
+        description: 'Discover the perfect hearing aid through our trial program. Experience cutting-edge technology and choose what best suits your lifestyle and hearing needs.'
+    },
+    {
+        image: './AboutUs/assessment.jpg',
+        title: 'Hearing Assessment',
+        description: 'Our comprehensive hearing evaluations utilize state-of-the-art tools to assess your hearing accurately. We deliver reliable insights to guide your journey toward better hearing.'
+    },
+    {
+        image: './AboutUs/dispensing.jpg',
+        title: 'Hearing Aid Dispensing',
+        description: 'Choose from a wide range of premium hearing aids. Our specialists ensure you find the ideal fit, offering both functionality and comfort for your everyday life.'
+    },
+    {
+        image: './AboutUs/therapy.jpg',
+        title: 'Speech Assessment & Therapy',
+        description: 'Boost communication skills with expert speech therapy. We provide tailored programs for children and adults, addressing speech, language, and communication challenges effectively.'
+    },
+    {
+        image: './AboutUs/service.jpg',
+        title: 'Hearing Aid Service',
+        description: 'Keep your hearing aids in top condition with our maintenance and repair services. From cleaning to troubleshooting, we’ve got you covered.'
+    },
+    {
+        image: './AboutUs/batteries.jpg',
+        title: 'Hearing Aid Batteries',
+        description: 'Stay powered with our selection of reliable and long-lasting hearing aid batteries. Convenience and quality, always available.'
+    }
+];
