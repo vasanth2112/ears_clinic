359f20f5732ea9bbfb6ca180c42d6f00f9c913e5
BenilDavid <DoodleDavid>
2024-12-15 17:36:10 +0530

    updated responsiveness for header main , about us, brands. added marquee effect

--- src/components/Brand/Brand.jsx
+++ src/components/Brand/Brand.jsx
@@ -1,17 +1,32 @@
-import '../../components/Brand/Brand.scss';
-import { BRAND_IMAGES } from '../../ListConstant';
+import "../../components/Brand/Brand.scss";
+import { BRAND_IMAGES } from "../../ListConstant";
+import Marquee from "react-fast-marquee";
 
 export default function Brand() {
-    return (<>
-        <div className='brand_container'>
-            <h1><span className='letter_color'>T</span>rusted <span className='letter_color'>B</span>rands <span className='letter_color'>W</span>e <span className='letter_color'>P</span>artner <span className='letter_color'>W</span>ith</h1>
-            <div className="brand">
-                {BRAND_IMAGES.map((content, index) => (
-                    <div className="brand_img">
-                        <img src={content.img}></img>
-                    </div>
-                ))}
-            </div>
+  return (
+    <>
+      <div className="brand_container">
+        <h1>
+          <span className="letter_color">T</span>rusted{" "}
+          <span className="letter_color">B</span>rands{" "}
+          <span className="letter_color">W</span>e{" "}
+          <span className="letter_color">P</span>artner{" "}
+          <span className="letter_color">W</span>ith
+        </h1>
+        <div className="brand">
+          {/* <div className="marquee-container">
+            <div className="marquee"> */}
+          <Marquee>
+            {BRAND_IMAGES.map((content, index) => (
+              <div key={index} className="brand_img">
+                <img src={content.img} alt={`Brand ${index}`} />
+              </div>
+            ))}
+          </Marquee>
+          {/* </div>
+          </div> */}
         </div>
-    </>)
+      </div>
+    </>
+  );
 }
