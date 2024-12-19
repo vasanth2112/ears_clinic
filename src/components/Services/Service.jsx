897719e423a0bf6f7313f9e10e65e8f8808a8d36
vasanth <vasanthsbtect@gmail.com>
2024-12-19 20:52:16 +0530

    commit

--- src/components/Services/Service.jsx
+++ src/components/Services/Service.jsx
@@ -3,10 +3,30 @@ import '../../components/Services/Service.scss';
 import { SERVICES } from '../../ListConstant';
 import { Button } from "@mui/material";
 import ArrowForwardIcon from '@mui/icons-material/ArrowForward';
+import { useEffect, useRef } from "react";
+import Lottie from "lottie-web";
+import service from "../../../public/AboutUs/service.json";
 
 export default function Service() {
+    const serviceAnimation = useRef(null);
+
+    useEffect(() => {
+      const svcAnimation = Lottie.loadAnimation({
+        container: serviceAnimation.current,
+        renderer: "svg",
+        loop: true,
+        autoplay: true,
+        animationData: service,
+      });
+  
+      return () => {
+        svcAnimation.destroy();
+      };
+    }, []);
+
     return (<>
         <div id='service' className='aboutus_container_commitment'>
+        <div ref={serviceAnimation} className="serviceAnime"></div>
             <div className='aboutus_container_commitment_flex'>
                 <h1><span className='letter_color'>O</span>ur <span className='letter_color'>S</span>ervices</h1>
 
